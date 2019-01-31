c6dd114355fa9f81a151fddcba7e4e1c75042356
Dreamwalker <aristojeff@gmail.com>
2019-01-31 21:36:40 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,45 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 200.0,
                 width: double.infinity,
                 color: Color(0xfffa624f),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: IconButton(
+                  icon: IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    onPressed: () {},
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 125.0,
+                left: 15.0,
+                right: 15.0,
+                child: Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(7.0),
+                  child: Container(
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7.0),
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 75.0,
+                left: MediaQuery.of(context).size.width / 2 - 50.0,
+                child: Container(
+                  height: 100.0,
+                  width: 100.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    image: DecorationImage(image: AssetImage(""),
+                    fit: BoxFit.cover)
+                  ),
+                ),
               )
             ],
           )
@@ -41,4 +80,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-

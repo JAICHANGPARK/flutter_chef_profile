66261175ec7977599615bfd233f76fa264eb6507
Dreamwalker <aristojeff@gmail.com>
2019-01-31 21:42:45 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -54,23 +54,44 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Container(
                     height: 200.0,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7.0),
-                      color: Colors.white
-                    ),
+                        borderRadius: BorderRadius.circular(7.0),
+                        color: Colors.white),
                   ),
                 ),
               ),
               Positioned(
                 top: 75.0,
-                left: MediaQuery.of(context).size.width / 2 - 50.0,
+                left: (MediaQuery.of(context).size.width / 2) - 50.0,
                 child: Container(
                   height: 100.0,
                   width: 100.0,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.0),
-                    image: DecorationImage(image: AssetImage(""),
-                    fit: BoxFit.cover)
-                  ),
+                      borderRadius: BorderRadius.circular(50.0),
+                      image: DecorationImage(
+                          image: AssetImage(""), fit: BoxFit.cover)),
+                ),
+              ),
+              Positioned(
+                top: 190.0,
+                left: (MediaQuery.of(context).size.width / 2) - 105.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Dreamwalker",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 17.0),
+                    ),
+                    SizedBox(height: 7.0,),
+                    Text(
+                      'China', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0,
+                      color: Colors.grey,
+                    ),
+                    ),
+                    SizedBox(height: 10.0,)
+                  ],
                 ),
               )
             ],
